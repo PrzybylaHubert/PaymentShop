9ab2af1049e43a49060a6058d9581e8f6d890638
PrzybylaHubert <hubercik022@gmail.com>
2023-01-20 13:57:16 +0100

    Create fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,31 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\Beer;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class AppFixtures extends Fixture
 {
+    private const ENDPOINT = 'https://api.punkapi.com/v2/beers';
+
+    public function __construct(private HttpClientInterface $client) {} 
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $response = $this->client->request('GET', self::ENDPOINT);
+        $beers = json_decode($response->getContent());
+        $i = 0;
+        foreach($beers as $beer) {
+            $object[$i] = new Beer();
+            $object[$i]->setName($beer->name);
+            $object[$i]->setDescription($beer->description);
+            $object[$i]->setPrice(rand(100, 1000));
+            $object[$i]->setImageUrl($beer->image_url);
+            $manager->persist($object[$i]);
+            $i++;
+        }
         $manager->flush();
     }
 }
